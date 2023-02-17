08ca4875c33f71b61952229c06a28bbe546363ab
Stan-is-the-man <stanislav.yanakievv@gmail.com>
2023-02-17 14:46:00 +0200

    series

--- main.py
+++ main.py
@@ -10,4 +10,25 @@ my_data = pd.DataFrame(cars_models)
 print(my_data)
 
 print(f"Current version of PANDAS is {(pd.__version__)}")
+###########################
+
+a = [1, 7, 2, 5, 3, 4, 6, 8, 9]
+myvar = pd.Series(a)
+print(myvar)
+
+#################
+
+a = ['Stan', 'The', 'Man']
+myvar = pd.Series(a)
+
+b = ['Stan', 'The', 'Man']
+myvar_b = pd.Series(a, index=['a', 'b', 'c'])
+
+
+print(myvar)
+print(myvar_b)
+######################
+
+
+
 
