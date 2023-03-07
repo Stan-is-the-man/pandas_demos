089f15993cdd286c34cd9c3b88785968f8cf7aee
Stan-is-the-man <stanislav.yanakievv@gmail.com>
2023-03-07 16:34:23 +0200

    check

--- main.py
+++ main.py
@@ -180,6 +180,7 @@ from pandas import read_csv
 # df['Calories'].fillna(100, inplace=True)
 # print(df.to_string())
 
-a = {1,2,3}
-b={3 ,4, 5}
-print(a - b)
+# a = {1,2,3}
+# b={3 ,4, 5}
+# print(a - b)
+
